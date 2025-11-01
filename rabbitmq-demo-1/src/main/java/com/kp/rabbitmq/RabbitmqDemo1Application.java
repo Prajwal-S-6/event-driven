8df31cb4dc83428855686a189bee0da7271fd341
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-11-01 07:58:57 +0530

    Add initial project files including POM and application classes

---
+++ rabbitmq-demo-1/src/main/java/com/kp/rabbitmq/RabbitmqDemo1Application.java
@@ -0,0 +1,13 @@
+package com.kp.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqDemo1Application.class, args);
+    }
+
+}
