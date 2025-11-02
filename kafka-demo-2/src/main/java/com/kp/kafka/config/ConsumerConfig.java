cd5f46b6690c21c9abf5259c0ea044f379830563
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-11-02 10:38:45 +0530

    Add Order consumer configuration and logging for consumed orders

--- kafka-demo-2/src/main/java/com/kp/kafka/config/ConsumerConfig.java
+++ kafka-demo-2/src/main/java/com/kp/kafka/config/ConsumerConfig.java
@@ -1,15 +1,15 @@
-package com.kp.kafka.consumer;
+package com.kp.kafka.config;
 
 import com.kp.kafka.dto.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
-@Component
+@Configuration
 @Slf4j
-public class OrderConsumer {
+public class ConsumerConfig {
 
     @Bean
     public Consumer<Order> orderConsumer() {
