a64cbb6a9bc0c68d67fe26379f85faa52a21df30
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-11-02 10:51:50 +0530

    Add user consumer configuration and implementation for Kafka integration

--- kafka-demo-2/src/main/java/com/kp/kafka/config/ConsumerConfig.java
+++ kafka-demo-2/src/main/java/com/kp/kafka/config/ConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.kp.kafka.config;
 
 import com.kp.kafka.dto.Order;
+import com.kp.kafka.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +15,10 @@ public class ConsumerConfig {
     @Bean
     public Consumer<Order> orderConsumer() {
         return (order) -> log.info("Consumed Order: {}", order);
+    }
+
+    @Bean
+    public Consumer<User> userConsumer() {
+        return (user) -> log.info("Consumed User: {}", user);
     }
 }
