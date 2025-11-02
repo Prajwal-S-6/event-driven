764756d08d4dde9911e622bc4e0030c315376147
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-11-02 11:28:29 +0530

    Add Order consumer implementation to handle and log incoming Order messages

--- kafka-demo-1/src/main/java/com/kp/kafka/consumer/KafkaConsumer.java
+++ kafka-demo-1/src/main/java/com/kp/kafka/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.kp.kafka.consumer;
 
+import com.kp.kafka.dto.Order;
 import com.kp.kafka.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,5 +18,10 @@ public class KafkaConsumer {
     @KafkaListener(topics = {"demo_topic_user"}, groupId = "demo_group")
     public void consumeUserDetails(User message) {
         log.info("Consumed User Details: {}", message);
+    }
+
+    @KafkaListener(topics = {"order_topic"}, groupId = "order_group-1", properties = {"spring.json.value.default.type=com.kp.kafka.dto.Order"})
+    public void consumeOrderDetails(Order order) {
+        log.info("Consumed Order Details: {}", order);
     }
 }
